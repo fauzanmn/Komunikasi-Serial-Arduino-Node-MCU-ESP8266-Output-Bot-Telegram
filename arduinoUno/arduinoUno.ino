e63a86a92431cbf957712f31b4049246a0a164fb
allblue <100438762+fauzanmn@users.noreply.github.com>
2023-06-06 11:57:10 +0700

    Update arduinoUno.ino

--- arduinoUno/arduinoUno.ino
+++ arduinoUno/arduinoUno.ino
@@ -1,26 +1,26 @@
-#include "DHT.h"
 
-#define DHTPIN 7
-#define DHTTYPE DHT11
+//*************** INISIALISASI LIBRARY YANG DIGUNAKAN ***************// 
+#include "DHT.h" // Library DHT
+#define DHTPIN 7 // Hubungkan pin Data Sensor DHT ke Pin 7 Arduino
+#define DHTTYPE DHT11 // Definisikan Tipe Sensor DHT yang digunakan; ex : DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); // Membuat objek variabel
+
+float h, t; // Membuat tipe data variabel; ex : float (bilangan desimal)
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
   
-  dht.begin();
+  Serial.begin(9600); // Atur baud rate Serial Monitor yang digunakan; ex : baud rate 9600
+  
+  dht.begin(); // Menjalankan library DHT
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-//  Serial.print("Kelembaban : ");
-  Serial.print(h);
-  Serial.print(",");
-  Serial.println(t);
-//  Serial.println("°C ");
-  delay(1000);
+
+  h = dht.readHumidity(); // Sensor Membaca Kelembapan ( % )
+  t = dht.readTemperature(); // Sensor Membaca Suhu ( °C )
+  Serial.print(h); // Menampilkan nilai Kelembapan di Serial Monitor
+  Serial.print(","); // Menampilkan simbol koma (,) sebagai pemisah antara data sensor
+  Serial.println(t); // Menampilkan nilai Suhu di Serial Monitor
+  delay(1000); // delay setiap 1000 milisekon (1 detik)
 }
